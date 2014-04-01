7da663d92e3ce995833fb5ba07e7dec8688829e2
google <google@fleischmann.hu>
2014-04-01 08:28:13 +0000

    CHG: added 'order by 1' to performance command.

--- tsmadm.pl/plugins/v2_plugin.pl
+++ tsmadm.pl/plugins/v2_plugin.pl
@@ -289,7 +289,7 @@ sub basicPerformanceFromSummary ( $$$ ) {
        @query = &runTabdelDsmadmc( "select date(START_TIME),time(START_TIME),date(END_TIME),time(END_TIME),NUMBER,ENTITY,SCHEDULE_NAME,EXAMINED,AFFECTED,FAILED,BYTES,IDLE,MEDIAW,PROCESSES,SUCCESSFUL,cast((END_TIME-START_TIME) seconds as decimal) from summary where ACTIVITY='".$activity."' and (start_time >= current_timestamp - $today day) and (end_time <= current_timestamp - $fromday day)" );
     } else {
        #@query = &runTabdelDsmadmc( "select date(START_TIME),time(START_TIME),date(END_TIME),time(END_TIME),NUMBER,ENTITY,SCHEDULE_NAME,EXAMINED,AFFECTED,FAILED,BYTES,IDLE,MEDIAW,PROCESSES,SUCCESSFUL,cast(timestampdiff(2,char((END_TIME-START_TIME))) as decimal) from summary where ACTIVITY='".$activity."' and (start_time >= current_timestamp - $today day) and (end_time <= current_timestamp - $fromday day)" );
-       @query = &runTabdelDsmadmc( "select date(START_TIME),time(START_TIME),date(END_TIME),time(END_TIME),NUMBER,ENTITY,SCHEDULE_NAME,EXAMINED,AFFECTED,FAILED,BYTES,IDLE,MEDIAW,PROCESSES,SUCCESSFUL,timestampdiff(2,char((END_TIME-START_TIME))) from summary where ACTIVITY='".$activity."' and (start_time >= current_timestamp - $today day) and (end_time <= current_timestamp - $fromday day)" );
+       @query = &runTabdelDsmadmc( "select date(START_TIME),time(START_TIME),date(END_TIME),time(END_TIME),NUMBER,ENTITY,SCHEDULE_NAME,EXAMINED,AFFECTED,FAILED,BYTES,IDLE,MEDIAW,PROCESSES,SUCCESSFUL,timestampdiff(2,char((END_TIME-START_TIME))) from summary where ACTIVITY='".$activity."' and (start_time >= current_timestamp - $today day) and (end_time <= current_timestamp - $fromday day) order by 1" );
     }
     return 0 if ( $#query < 0 || $LastErrorcode );
 
